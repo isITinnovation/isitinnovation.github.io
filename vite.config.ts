91e84d5471f0b754b2fee0ddfac5817d422e641d
isITinnovation <isitinnovation@gmail.com>
2025-02-28 15:30:28 +0900

    수정정중

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // 상대 경로로 변경
+  base: "/isITinnovation/",
   server: {
     port: 3000,
   },
