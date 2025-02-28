28d5c8d5870301293bd1906f7bac79b64d87d52c
isITinnovation <isitinnovation@gmail.com>
2025-02-28 17:12:18 +0900

    pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // 반드시 '/'로 설정
+  base: "/isitBlog/", // 저장소 이름
   server: {
     port: 3000,
   },
   build: {
-    outDir: "dist",
+    outDir: "dist", // 빌드 출력 디렉토리
     assetsDir: "assets",
+    emptyOutDir: true,
   },
 });
