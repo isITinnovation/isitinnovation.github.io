602fab61b2969eaa2ab4fdd061aed6d929131be8
isITinnovation <isitinnovation@gmail.com>
2025-02-28 16:22:44 +0900

    Revert "수정정중"
    This reverts commit 91e84d5471f0b754b2fee0ddfac5817d422e641d.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/isITinnovation/",
+  base: "./", // 상대 경로로 변경
   server: {
     port: 3000,
   },
