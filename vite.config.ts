c6b7c281e8159ca1faae6055fd046cfe7406a7f1
isITinnovation <isitinnovation@gmail.com>
2025-02-28 16:22:59 +0900

    Revert "힘들다 ㅠ"
    This reverts commit 2a2784ed461ef25a13027b1eb1b204f13a061bc0.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // GitHub Pages를 위한 base 설정
+  base: "./", // 반드시 '/'로 설정
   server: {
     port: 3000,
   },
