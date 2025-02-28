9c4082dca2b53cb777aea34dc067c3aff649b33f
isITinnovation <isitinnovation@gmail.com>
2025-02-28 14:56:53 +0900

    재수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // 반드시 '/'로 설정
+  base: "./", // 반드시 '/'로 설정
+  server: {
+    port: 3000,
+  },
   build: {
     outDir: "dist",
+    assetsDir: "assets",
   },
 });
