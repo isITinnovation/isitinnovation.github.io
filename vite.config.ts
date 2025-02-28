120450201b015e03eea52fc80b7cc54651a2fe59
isITinnovation <isitinnovation@gmail.com>
2025-02-28 17:04:31 +0900

    다시 배포

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/isitBlog/", // 저장소 이름
+  base: "./", // 반드시 '/'로 설정
   server: {
     port: 3000,
   },
   build: {
     outDir: "dist",
     assetsDir: "assets",
-    emptyOutDir: true,
   },
 });
