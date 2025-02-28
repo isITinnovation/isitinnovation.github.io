5cb521b3116fca6365c0a0f34920e3392384d1a8
isITinnovation <isitinnovation@gmail.com>
2025-02-28 14:22:30 +0900

    isit-03-vite 설정 조정

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // GitHub Pages 루트 경로
+  build: {
+    outDir: "dist",
+  },
   resolve: {
     alias: {
       "@": "/src",
